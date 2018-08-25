5d48d63fb6c6f0da6798b728d741b67e537d0863
AS400JPLPC <laroche.jeanpierre@gmail.com>
2018-08-25 07:45:47 +0200

    nouveau fichier : pgm_and_pgm.png 	nouveau fichier : tree.png 	renommé :         VteTERM.cpp -> vteterm.cpp

--- vteterm.cpp
+++ vteterm.cpp
@@ -3,10 +3,10 @@
 /// projet 2018-08-08  (C) 2018   Copyright 2018  <laroche.jeanpierre@gmail.com>
 /// but : 	terminal rapide	/ flexible / respectant le code escape
 ///			fast / flexible terminal / respecting the escape code
-/// 		console dédié uniquement à une application multi-process 
+/// 		console dédié uniquement à une application multi-process
 ///			console dedicated to an application only multi-process
 ///			-no-pie EXÉCUTABLE  programme maitre mini server terminal
-///			+ souple que XTERM et + sécuritaire que les terminal public  pour des applicatifs 
+///			+ souple que XTERM et + sécuritaire que les terminal public  pour des applicatifs
 
 
 
@@ -35,12 +35,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -51,16 +51,17 @@
 
 ///------------------------------------------
 /// paramétrage spécifique
-/// ex: 
+/// ex:
 ///------------------------------------------
 
-#define WORKLIB		"/home/soleil/VTERM/pgm/"
+#define WORKLIB		"/home/soleil/VTETERM/pgm/"
 #define WORKPGM		"./term"
 
 /// security key as a parameter for the application
-#define KEYPGM 		"GEN001K"	/// example 
+#define KEYPGM 		"GEN001K"	/// example
 
 bool _DEBUG_  = true; /// ALT_F4 ATVIVE  _DEBUG_ = true
+
 #define MESSAGE_ALT_F4 "vous devez activer uniquement \n en développemnt  \n Confirm destroy Application --> DEBUG"
 
 
@@ -73,13 +74,14 @@ bool _DEBUG_  = true; /// ALT_F4 ATVIVE  _DEBUG_ = true
 #define VTENAME "VTE-TERM3270"
 
 #define COL		132
+
 #define ROW		43  ///including a line for the system
 
 /// defined not optional
 #define VTEFONT	"DejaVu Sans Mono"
- 
+
 //*******************************************************
-// PROGRAME 
+// PROGRAME
 //*******************************************************
 
 #include <clocale>
@@ -95,7 +97,7 @@ bool _DEBUG_  = true; /// ALT_F4 ATVIVE  _DEBUG_ = true
 
 #include <sys/wait.h>
 #include <sys/ipc.h>
- 
+
 
 
 
@@ -136,11 +138,11 @@ int exec_prog(const char* commande)
 ///-------------------------------------
 gboolean key_press_ALTF4()
 {
-	if (_DEBUG_ == 1)	
+	if (_DEBUG_ == 1)
 	{
 		GtkWidget *dialog;
 		const gchar* _MSG_ =  MESSAGE_ALT_F4;
-			
+
 		dialog = gtk_message_dialog_new(
 										 GTK_WINDOW(window),
 										 GTK_DIALOG_MODAL,
@@ -149,11 +151,11 @@ gboolean key_press_ALTF4()
 										 _MSG_,
 										 NULL,
 										 NULL);
-										 
+
 		int result = gtk_dialog_run (GTK_DIALOG (dialog));
-		
+
 		gtk_widget_destroy(GTK_WIDGET(dialog));
-		
+
 		switch (result)
 		{
 			case  GTK_RESPONSE_YES:
@@ -189,7 +191,7 @@ void term_spawn_callback(VteTerminal *vte, GPid pid, GError *error, gpointer use
 
 
 /// -----------------------------------------------------------------------------
-/// personalisation projet utilisant une terminal simplifier pour de la gestion 
+/// personalisation projet utilisant une terminal simplifier pour de la gestion
 /// -----------------------------------------------------------------------------
 
 void	init_Terminal()
@@ -206,19 +208,19 @@ void	init_Terminal()
 	else if ( s->width <= 1920 && s->height >=1080 ) sprintf(font_terminal,"%s %s" ,VTEFONT,"15"); 		/// correct
 	else if ( s->width > 1920  ) sprintf(font_terminal,"%s %s" ,VTEFONT,"16");							/// confortable and extend numbers columns and rows
 
-	
-	// resize  title  font  
+
+	// resize  title  font
     VTE = VTE_TERMINAL (terminal);
-     
+
 	vte_terminal_set_size (VTE, COL, ROW);													/// size du terminal
-  
+
 	gtk_window_set_title(GTK_WINDOW(window), VTENAME);										/// titre du terminal de base
 
 	vte_terminal_set_font (VTE,pango_font_description_from_string(font_terminal));			/// font utilisé
 
 	vte_terminal_set_scrollback_lines (VTE,0);		 										///	désactiver le défilement arrière.
 
-    vte_terminal_set_encoding(VTE,NULL,NULL);												/// UTF8                           
+    vte_terminal_set_encoding(VTE,NULL,NULL);												/// UTF8
 }
 
 
@@ -252,21 +254,31 @@ int main(int argc, char *argv[])
 {
 	std::setlocale(LC_ALL, "");
 
-	// program library to call   
+
+	/// contrôle autorisation traitement --> protection 
+	const char * _key = argv[2] ;
+	if (argc > 1  &&  strcmp(_key , KEYPGM ) != 0) return EXIT_FAILURE;
+
 	const gchar *dir = WORKLIB;
 
-	// program and parm
-	gchar *command[] = { (gchar*)WORKPGM, (gchar*)KEYPGM, NULL};
-
-
-
-
+	gchar ** command ;
+	gchar *arg_1[] = { (gchar*)WORKPGM, (gchar*)KEYPGM, NULL};
+	gchar *arg_4[] = { (gchar*) argv[1], (gchar*)KEYPGM ,(gchar*)argv[3], NULL};		/// arg[1] P_pgm , [3] Pid_msgq
+	/// exemple P_Pgm (argv[1]) = ./programme   Pid_msgq (argv[3]) = 0000721489 
 	
+	if (argc == 1 )  command = arg_1;
+
+	if (argc == 4 )  command = arg_4;
+
+	if (argc > 4)  return EXIT_FAILURE;
+
 	
 	// Initialise GTK, the window traditional work
-    gtk_init(&argc, &argv);
+	 
+    gtk_init(&argc,&argv);
+ 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "VteTerm");
+    gtk_window_set_title(GTK_WINDOW(window), "VTERM0");
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ALWAYS);
 	gtk_window_set_resizable (GTK_WINDOW(window),false);
 	gtk_window_set_deletable (GTK_WINDOW(window),false);
@@ -275,24 +287,24 @@ int main(int argc, char *argv[])
 
     /* Initialise the terminal */
     terminal = vte_terminal_new();
- 
+
     vte_terminal_spawn_async(
 		VTE_TERMINAL(terminal), //VteTerminal *terminal
         VTE_PTY_DEFAULT, // VtePtyFlags pty_flags,
-        dir,			// const char *working_directory  
-        command,		// command  
-        NULL,			// environment  
-         (GSpawnFlags)(G_SPAWN_SEARCH_PATH |G_SPAWN_FILE_AND_ARGV_ZERO),	// spawn flags  
+        dir,			// const char *working_directory
+        command,		// command
+        NULL,			// environment
+         (GSpawnFlags)(G_SPAWN_SEARCH_PATH |G_SPAWN_FILE_AND_ARGV_ZERO),	// spawn flags
         NULL,			// GSpawnChildSetupFunc child_setup,
         NULL,			// gpointer child_setup_data,
-        NULL,			// GDestroyNotify child_setup_data_destroy, 
+        NULL,			// GDestroyNotify child_setup_data_destroy,
         -1,				//  int timeout
         NULL,			// GCancellable *cancellable,
-        
+
         term_spawn_callback,	// VteTerminalSpawnAsyncCallback callback, get pid child
-        
+
         NULL);			// gpointer user_data
- 
+
 
     // Connect some signals
 	g_signal_connect(GTK_WINDOW(window),"delete_event", G_CALLBACK (key_press_ALTF4), NULL);
@@ -300,17 +312,17 @@ int main(int argc, char *argv[])
 	g_signal_connect(terminal, "window-title-changed", G_CALLBACK(on_title_changed), NULL);
 	g_signal_connect(terminal, "resize-window", G_CALLBACK(on_resize_window),NULL);
 
-	
+
 
     // specific initialization of the terminal
 	init_Terminal();
-	
+
     /* Put widgets together and run the main loop */
     gtk_container_add(GTK_CONTAINER(window), terminal);
-    
+
     gtk_widget_show_all(window);
-    
+
     gtk_main();
-    
+
     return EXIT_SUCCESS;
 }
